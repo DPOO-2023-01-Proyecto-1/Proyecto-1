ad4f71a0fdccd384295d913b71107a60099e78fa
Manuel Gómez <m.gomez49@uniandes.edu.co>
2023-04-01 11:21:52 -0500

    Función de generar id y checkear si el id ya estaba

--- Hotel_Gargola/src/modelo/Recepcionista.java
+++ Hotel_Gargola/src/modelo/Recepcionista.java
@@ -1,10 +1,34 @@
 package modelo;
 
+import java.util.Map;
+import java.util.Random;
+
 public class Recepcionista extends Usuario {
 
 	public Recepcionista(String login, String password, String userType) {
 		super(login, password, userType);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Integer generarIdParaReservasNuevas( Map<Integer, Reserva> mapaReservas)
+	{	//para generar el id de las reservas toca que entre el mapa para que se checkee que el id no esté creado ya
+		Random random = new Random();
+		Boolean checkearId = true;
+		int idGenerado = 0;
+		while (checkearId = true)
+		{
+			idGenerado = random.nextInt(1000);// aqui genera el id
+			if (mapaReservas.get(idGenerado)!= null)
+			{
+				idGenerado = random.nextInt(1000);
+			}
+			else
+			{
+				checkearId = false;
+			}
+		}
+		
+		return idGenerado;
 	}
 
 }
