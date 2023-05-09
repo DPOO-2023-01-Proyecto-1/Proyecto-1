f8625244845b92d9b64b637478624c282330ef30
Manuel Gómez <m.gomez49@uniandes.edu.co>
2023-05-08 22:18:27 -0500

    mapa de meses como se debe listo

--- Hotel_Gargola/src/interfaz/VentanaListaDeReservas.java
+++ Hotel_Gargola/src/interfaz/VentanaListaDeReservas.java
@@ -7,22 +7,77 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.Graphics;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class VentanaListaDeReservas extends JFrame implements ActionListener{
 	
 	JFrame frame = new JFrame();
 	JButton regresar;
+	JPanel panelCentro;
+	String[] meses = {"Enero", "Febrero", "Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+	int[] valores = {1000, 2500, 400, 7000, 3300, 2000, 9000, 2500, 3000, 1600, 7000, 1200};
+	int maxValue = 10000;
+	Map<Integer,Integer> mapaMeses = new LinkedHashMap<>();
 	VentanaRecepcionista vRecepcionista;
 	
 	VentanaListaDeReservas(VentanaRecepcionista vRecepcionista1)
-	{
+	{	
+		//----------CUADRA EL HASH MAP-----------//
+		try {
+			BufferedReader brReservas;
+			String linea = "";
+			brReservas = new BufferedReader(new FileReader("./data/reservas.txt"));
+			while ((linea = brReservas.readLine()) != null) 
+			{
+				String[] partes = linea.split(";"); // Separa la linea por los ;
+				String[] partes2 = partes[1].split("-");
+				int mesesito = Integer.parseInt(partes2[1]);
+				if (mapaMeses.get(mesesito)==null)
+				{
+					int valorInicial = 1;
+					mapaMeses.put(mesesito, valorInicial);
+				}
+				else
+				{
+					int valorsito = mapaMeses.get(mesesito) +1;
+					mapaMeses.put(mesesito, valorsito);
+				}
+
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		//--------------------------------------//
 		vRecepcionista = vRecepcionista1;
 		frame.setTitle("Ver Lista De Reservas");
 		frame.setSize(new Dimension(750,750));
-		
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		//------------CUADRAR LABEL------------//
 		regresar =  new JButton("Regresar");
@@ -43,12 +98,12 @@ public class VentanaListaDeReservas extends JFrame implements ActionListener{
 	}
 	
 	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource()==regresar)
-		{	
-			
+		{
 			vRecepcionista.getFrameRecepcionista().setVisible(true);
 			frame.dispose();
 		}
